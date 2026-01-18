37b3e64404c694d4c0d1db737f483c45d4c55949
lucaskmpz <lucasclmaia0@gmail.com>
2026-01-17 23:26:10 -0300

    feat: adding Swagger and global ValidationPipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -14,6 +15,8 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api/docs', app, document)
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
   await app.listen(4000)
   console.log('Backend listening on http://localhost:4000')
